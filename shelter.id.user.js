dfb2d57c97418ba47e18792ce49ebc70f71d69f7
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-06-26 20:19:17 +0900

    Update shelter.id.user.js

--- shelter.id.user.js
+++ shelter.id.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
 // @name         쉘터 정확한 날자 및 시간 표시
 // @namespace    https://shelter.id/
-// @version      1.1.0
+// @version      1.2.0
 // @description  쉘터 정확한 날자 및 시간 표시
 // @author       MaGyul
 // @match        https://shelter.id/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shelter.id
-// @update       https://raw.githubusercontent.com/MaGyul/test/main/shelter.id.user.js
+// @updateURL    https://raw.githubusercontent.com/MaGyul/test/main/shelter.id.user.js
+// @downloadURL  https://raw.githubusercontent.com/MaGyul/test/main/shelter.id.user.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.addEventListener("DOMContentLoaded", updateDate);
+    var nextId = undefined;
+    var prevId = undefined;
+
+    document.addEventListener("DOMContentLoaded", main);
 
     // history onpushstate setup
     (function(history){
@@ -26,7 +30,97 @@
         };
     })(window.history);
 
-    history.onpushstate = updateDate;
+    history.onpushstate = main;
+
+    function main() {
+        findDom('.tit-refresh', (dom) => {
+            dom.onclick = () => {
+                fetchArticles('default');
+            };
+        });
+        findDom('button.prev', (dom) => {
+            dom.onclick = () => {
+                fetchArticles('prev');
+            };
+        });
+        findDom('button.next', (dom) => {
+            dom.onclick = () => {
+                fetchArticles('next');
+            };
+        });
+        updateDate();
+        fetchArticles('default');
+    }
+
+    function getPageSize() {
+        let dom = document.querySelector('.page-size');
+        var index = dom.selectedIndex;
+        if (index === 1) return 80;
+        if (index === 2) return 100;
+        return 40;
+    }
+
+    function getLastBoardId() {
+        let dom = document.querySelector('.board__body');
+        let lastEle = dom.lastElementChild;
+        return lastEle.getAttribute('data-id');
+    }
+
+    function fetchArticles(type) {
+        setTimeout(() => {
+            let pageSize = getPageSize();
+            if (document.querySelector('.board__body')?.children?.length < pageSize) {
+                return fetchArticles(type);
+            }
+            let shelterId = location.pathname.split('/')[1];
+            if (shelterId == 'planet') return;
+            // https://rest.shelter.id/v1.0/list-items/personal/gM7ZgsaLScbOwQ5eQ/shelter/articles?offset_id=433524&size=40 next
+            // https://rest.shelter.id/v1.0/list-items/personal/gM7ZgsaLScbOwQ5eQ/shelter/articles?prev_id=433521&size=40 prev
+            switch(type) {
+                case 'next':
+                    fetch(`https://rest.shelter.id/v1.0/list-items/personal/${shelterId}/shelter/articles?${nextId ? 'offset_id=' + nextId + '&' : ''}size=${pageSize}`)
+                        .then(r => r.json()).then(updateDateArticles);
+                    break;
+                case 'prev':
+                    fetch(`https://rest.shelter.id/v1.0/list-items/personal/${shelterId}/shelter/articles?${prevId ? 'prev_id=' + prevId + '&' : ''}size=${pageSize}`)
+                        .then(r => r.json()).then(updateDateArticles);
+                    break;
+                default:
+                    fetch(`https://rest.shelter.id/v1.0/list-items/personal/${shelterId}/shelter/articles?size=${pageSize}`)
+                        .then(r => r.json()).then(updateDateArticles);
+                    break;
+            }
+        }, 500);
+    }
+
+    function updateDateArticles(data) {
+        if (data.has_next) {
+            nextId = data.list[data.list.length - 1].id;
+        }
+        if (data.has_prev) {
+            prevId = data.list[0].id;
+        }
+
+        for (let item of data.list) {
+            let ele = [...document.querySelectorAll(`app-board-list-item[data-id="${item.id}"] > .SHELTER_COMMUNITY`)].at(-1);
+            if (ele) {
+                let create_ele = ele.querySelector('.create');
+                let create_date = new Date(item.create_date);
+                let year = ('' + create_date.getFullYear()).substr(2);
+                let month = change9under(create_date.getMonth() + 1);
+                let date = change9under(create_date.getDate());
+                let hours = change9under(create_date.getHours());
+                let minutes = change9under(create_date.getMinutes());
+                let seconds = change9under(create_date.getSeconds());
+                // 생성된 날자가 오늘일 경우
+                if ((new Date).getDate() == date) {
+                    create_ele.textContent = `${hours}:${minutes}:${seconds}`;
+                } else {
+                    create_ele.textContent = `${year}-${month}-${date}`;
+                }
+            }
+        }
+    }
 
     function updateDate() {
         setTimeout(() => {
@@ -47,5 +141,23 @@
         }, 200);
     }
 
-    updateDate();
+    function change9under(i) {
+        if (i <= 9) {
+            i = '0' + i;
+        }
+        return i;
+    }
+
+    function findDom(path, callback) {
+        setTimeout(() => {
+            let dom = document.querySelector(path);
+            if (dom) {
+                callback(dom);
+                return;
+            }
+            findDom(path, callback);
+        }, 500);
+    }
+
+    main();
 })();
